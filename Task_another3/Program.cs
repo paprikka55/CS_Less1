c15cb030616ed16b20e2e2aaf429285f9a0fb892
Alexander Malyatin <paprikka84@gmail.com>
2024-02-08 11:15:40 +0600

    added 4 homework tasks

---
+++ Task_another3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+      int rem = number % 2;
+      if ( rem == 0)
+        { Console.WriteLine("четное"); }
+      else
+        { Console.WriteLine("нечетное"); }
+    }
+  
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}
---
+++ Task_another4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Answer
+{
+  static void PrintEvenNumbers(int number)
+  {
+    // Введите свое решение ниже
+    int index = 1;
+    int rem = 0;
+    while (index <= number)
+      {
+        rem = index % 2;
+        if (rem == 0)
+          {
+            Console.Write(index + " ");
+          }
+        index++;
+      }
+  }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+  static public void Main(string[] args)
+  {
+    int number;
+
+    if (args.Length >= 1)
+    {
+      number = int.Parse(args[0]);
+    }
+    else
+    {
+      // Здесь вы можете поменять значения для отправки кода на Выполнение
+      number = 102;
+    }
+
+    // Не удаляйте строки ниже
+    PrintEvenNumbers(number);
+  }
+}
