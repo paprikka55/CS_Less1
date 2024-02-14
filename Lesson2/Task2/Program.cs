e063f95ca4cc58688b80ca320a94029aecd2c8e7
Alexander Malyatin <paprikka84@gmail.com>
2024-02-14 12:08:48 +0600

    Added Lesson2:Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int index = 0;
+while (index < arr.Length)
+  {
+    arr[index] = index + 1;
+    Console.Write($"{arr[index]} ");
+    index++;
+  }
--- Lesson2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Lesson2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee17d609264f57c4be5e3ff5e79b48285cb5f43f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
