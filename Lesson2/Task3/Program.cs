3b1eab6e510517ea2f23178f48d71e63813845d6
Alexander Malyatin <paprikka84@gmail.com>
2024-02-14 12:32:13 +0600

    Added Lesson2:Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rnd = new Random();
+int[] digits = new int[10];
+int index = 0;
+while (index < digits.Length)
+{
+  digits[index] = rnd.Next(0, 100);
+  if (digits[index] % 2 == 0)
+  {
+    Console.Write($"{digits[index]} ");
+  }
+  index++;
+}
+
