70f59c8a77bdc16128b9ca557c65bd6df6f0b995
Alexander Malyatin <paprikka84@gmail.com>
2024-02-21 13:49:15 +0600

    Add Lesson3:HW1

--- Lesson3/HW1/Program.cs
+++ Lesson3/HW1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  { 
+    arr[i] = new Random().Next(1, 100); 
+    // arr[i] = i + 1;
+  }
+}
+
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  { 
+    if (i + 1 == arr.Length)
+    Console.WriteLine($"{arr[i]}");
+    else
+    Console.Write($"{arr[i]} "); 
+  }
+}
+
+int GetSumOfElements(int[] arr)
+{
+  int summ = 0;
+  for (int i = 0; i < arr.Length; i++)
+  { summ += arr[i]; }
+  return summ;
+}
+
+int GetProductOfElements(int[] arr)
+{
+  int prod = 1;
+  for (int i = 0; i < arr.Length; i++)
+  { prod = prod * arr[i]; }
+  return prod;
+}
+
+int n = 10;
+int[] arr = new int[n];
+FillArray(arr);
+PrintArray(arr);
+int sum = GetSumOfElements(arr);
+int product = GetProductOfElements(arr);
+Console.WriteLine(sum);
+Console.WriteLine(product);
