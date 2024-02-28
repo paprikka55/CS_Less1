3e072aea273beab534b677a8d6396f48a6fb204d
Alexander Malyatin <paprikka84@gmail.com>
2024-02-28 09:21:55 +0600

    Add Lesson4

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿string GetLetters(string str)
+{
+  string letters = "";
+  foreach(char e in str)
+  {
+    if (char.IsAsciiLetter(e) == true)
+    {
+      letters += e;
+    }
+  }
+  return letters;
+}
+
+
+Console.Write("Введите строку: ");
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
