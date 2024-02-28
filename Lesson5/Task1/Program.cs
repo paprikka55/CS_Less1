5e17f5955078534a037b496f701a658af6756794
Alexander Malyatin <paprikka84@gmail.com>
2024-02-28 10:50:40 +0600

    Add Lesson5: Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetFactorial(int number)
+{
+  int result = 0;
+  if (number == 1 || number == 0)
+  { 
+    Console.WriteLine($"Stop: {number} ");
+    return 1; 
+  }
+  //Console.WriteLine(number);
+  result = number * GetFactorial(number - 1);
+  Console.WriteLine($"N: {number}, !N: {result}");
+  return result;
+}
+int num = 5;
+Console.WriteLine($"!{num} = {GetFactorial(num)}");
